3a1efcda4dc4313d64009232cb7d350572fdc0fa
Daniel Kim <danielkim@Daniels-MacBook-Air-2.local>
2018-08-06 13:51:25 -0400

    seeds file with inital projects in table

---
+++ models/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO ideas (title, descriptions, rating)  VALUES
+("Social Engineer", "Census, apis that gov / un has, connect ppl w/ resources in area. homelessness mapping to shelters. Statistics to resources.", 5),
+("Word Wars", "Typing practice game", 5),
+("Pinder", "Tinder for pet adoptions", 5),
+("Zig Rig", "Power distribution measurement / requirements", 5),
+("Drone Control", "Wirelessly control a drone using bluetooth api and MERN stack", 5)
